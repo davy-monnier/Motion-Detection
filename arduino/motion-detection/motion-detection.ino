15e503fa105ba0667f3ae5da42c67d6eb18c2b98
Ali Boutaleb <aliboutaleb@MacBook-Pro-de-Ali-2.local>
2019-04-18 10:43:19 +0200

    Add Blynk libraries and Wifi

--- arduino/motion-detection/motion-detection.ino
+++ arduino/motion-detection/motion-detection.ino
@@ -1,3 +1,20 @@
+// Librairies to include
+#include <BlynkSimpleEsp8266.h>
+#include <ESP8266WiFi.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+
+// Auth Token Blynk
+char auth[] = "26d5fb1bc7514e218718f94a868d8a6f";
+
+// WiFi credentials.
+char ssid[] = "iPhone de Wiz";
+char pass[] = "batard2.0";
+
+// Define NTP Client to get time
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "europe.pool.ntp.org", 7200);
+
 int pirPin = D4;    // Define pin of PIR sensor
 int pirValue = 0;   // Initialize value of PIR sensor
 int lightPin = A0;  // Define pin of light sensor           
@@ -6,6 +23,9 @@ int lightVal = 0;   // Initialize value of light sensor
 void setup() {
   // Debug console
   Serial.begin(115200);
+
+  // Initiate Blynk connection
+  Blynk.begin(auth, ssid, pass);
 
 }
 
@@ -26,11 +46,14 @@ void loop() {
   // Manage light
   if (lightVal < 100) {
    Serial.println("Level 10");
- } else if (lightVal < 200) {
+  } else if (lightVal < 200) {
    Serial.println("Level 09");
- } else if (lightVal < 300) {
+  } else if (lightVal < 300) {
    Serial.println("Level 08");
- }
+  }
+
+  // Run Blynk and timer
+  Blynk.run();
 
   // Delay of 1 sec
   delay(1000);
