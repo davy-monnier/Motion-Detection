26ec8db422f2188870ed9b7826693225328c9148
Ali Boutaleb <aliboutaleb@MacBook-Pro-de-Ali-2.local>
2019-04-18 10:37:58 +0200

    Add light sensor and print value

--- arduino/motion-detection/motion-detection.ino
+++ arduino/motion-detection/motion-detection.ino
@@ -1,5 +1,7 @@
-int pirPin = D4;  // Define pin of PIR sensor
-int pirValue = 0; // Initialize value of pir sensor
+int pirPin = D4;    // Define pin of PIR sensor
+int pirValue = 0;   // Initialize value of PIR sensor
+int lightPin = A0;  // Define pin of light sensor           
+int lightVal = 0;   // Initialize value of light sensor
 
 void setup() {
   // Debug console
@@ -11,12 +13,18 @@ void loop() {
   // Get value from PIR sensor
   pirValue = digitalRead(pirPin);
 
+  // Get value from light sensor
+  lightVal = analogRead(lightPin);
+
   // Manage motions
   if (pirValue == 1) {
     Serial.println("Motion detected");
   } else {
     Serial.println("No motion detected");
   }
+
+  // Manage light
+  Serial.println(lightVal);
 
   // Delay of 1 sec
   delay(1000);
